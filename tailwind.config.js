000ec23c0a421e4411cbd27d36326f4bf0547698
azimuddin2 <mohammadazimuddin274@gmail.com>
2023-09-25 20:13:42 +0600

    tailwind css component library change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-const withMT = require("@material-tailwind/react/utils/withMT");
-
-module.exports = withMT({
+module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
-    "path-to-your-node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
-    "path-to-your-node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
     extend: {},
+    colors: {
+      primary: '#fcd842',
+      secondary: '#171426',
+      neutral: '#ffffff',
+    },
   },
-  plugins: [],
-});
-
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}

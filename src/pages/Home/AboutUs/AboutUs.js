2f3258e36b42a37f0e74bf959dbecf17a0e431d0
azimuddin2 <mohammadazimuddin274@gmail.com>
2023-09-27 23:43:47 +0600

    added aboutUs section

---
+++ src/pages/Home/AboutUs/AboutUs.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import aboutUsImg from '../../../assets/Images/about-us.jpg';
+import { Link } from 'react-router-dom';
+
+const AboutUs = () => {
+    return (
+        <section className='max-w-screen-lg lg:mx-auto mx-5 my-10 lg:my-20'>
+            <div className='grid grid-cols-1 lg:grid-cols-2 gap-6 lg:gap-20'>
+                <div>
+                    <img src={aboutUsImg} alt="aboutUs" />
+                </div>
+                <div className='lg:mt-20'>
+                    <h1 style={{ color: '#f4f4f5' }} className='text-4xl uppercase font-family font-medium'>About Us</h1>
+                    <h2 className='mt-3 text-primary text-3xl uppercase font-family font-medium'>We are hero to Dream!</h2>
+                    <h2 className='mt-2 text-secondary text-2xl lg:text-3xl uppercase font-family font-medium'>Our team is here service you.</h2>
+                    <p className='mt-4 text-secondary text-sm leading-6 w-4/5'>A gym is a large room, usually containing special equipment, where people go to do physical exercise and get fit. The gym has exercise bikes and running machines. While some guests play golf, others work out in the hotel gym.</p>
+                    <Link to='/'
+                        className="bg-primary text-secondary mt-8 font-semibold uppercase inline-flex justify-center items-center py-2 px-8 text-base text-center rounded-sm hover:bg-yellow-200 font-family">
+                        Program Join
+                        <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                        </svg>
+                    </Link>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default AboutUs;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from './Banner/Banner';
 import GymInformation from './GymInformation/GymInformation';
+import AboutUs from './AboutUs/AboutUs';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <GymInformation></GymInformation>
+            <AboutUs></AboutUs>
         </div>
     );
 };

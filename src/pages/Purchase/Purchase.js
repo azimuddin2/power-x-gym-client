5d1be32ee80c914fdcbd5ef9e00e5772bbfc86b9
azimuddin2 <mohammadazimuddin274@gmail.com>
2023-10-03 23:25:28 +0600

    added stepper and step

--- src/pages/Purchase/Purchase.js
+++ src/pages/Purchase/Purchase.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import PageCover from '../Shared/PageCover/PageCover';
+import { Button, Step, Stepper, Typography } from '@material-tailwind/react';
 
 const Purchase = () => {
     const purchase = useLoaderData();
@@ -16,7 +17,41 @@ const Purchase = () => {
         <section>
             <PageCover title={purchase.name}></PageCover>
             <div className='max-w-screen-lg lg:mx-auto mx-5 my-12 lg:my-20'>
+                <div className="w-full py-4 px-8">
+                    <Stepper
+                        activeStep={activeStep}
+                        isLastStep={(value) => setIsLastStep(value)}
+                        isFirstStep={(value) => setIsFirstStep(value)}
+                    >
+                        <Step onClick={() => setActiveStep(0)}>
+                            <span className='flex justify-center items-center'>1</span>
+                        </Step>
+                        <Step onClick={() => setActiveStep(1)}>
+                            <span className='flex justify-center items-center'>2</span>
+                        </Step>
+                        <Step onClick={() => setActiveStep(2)}>
+                            <span className='flex justify-center items-center'>3</span>
+                        </Step>
+                    </Stepper>
 
+                    <div className="mt-20 flex justify-between">
+                        <Button
+                            className='bg-primary text-secondary font-bold'
+                            onClick={handlePrev}
+                            disabled={isFirstStep}
+                        >
+                            Prev
+                        </Button>
+                        <Button
+                            className='bg-primary text-secondary font-bold'
+                            onClick={handleNext}
+                            disabled={isLastStep}
+                        >
+                            Next
+                        </Button>
+                    </div>
+
+                </div>
             </div>
         </section>
     );

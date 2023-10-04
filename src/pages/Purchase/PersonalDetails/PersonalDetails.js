70a14a67fcfc783e49c98794f66e447be630d5d5
azimuddin2 <mohammadazimuddin274@gmail.com>
2023-10-04 23:03:24 +0600

    updated personal details

--- src/pages/Purchase/PersonalDetails/PersonalDetails.js
+++ src/pages/Purchase/PersonalDetails/PersonalDetails.js
@@ -3,7 +3,19 @@ import React from 'react';
 const PersonalDetails = () => {
     return (
         <div className='my-20'>
-            <h1 className='text-5xl'>First Step</h1>
+            <form>
+                <div>
+                    <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First name</label>
+
+                    <input
+                        type="text"
+                        id="first_name"
+                        class="border border-gray-300 text-gray-900 rounded-sm w-full p-3 focus:outline-none focus:border-primary focus:ring-1 focus:ring-primary "
+                        required
+                    />
+
+                </div>
+            </form>
         </div>
     );
 };

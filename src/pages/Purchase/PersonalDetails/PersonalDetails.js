013e12d971731fe5e238cb6fe3d89be6806ce129
azimuddin2 <mohammadazimuddin274@gmail.com>
2023-10-04 23:06:26 +0600

    updated done

--- src/pages/Purchase/PersonalDetails/PersonalDetails.js
+++ src/pages/Purchase/PersonalDetails/PersonalDetails.js
@@ -15,6 +15,17 @@ const PersonalDetails = () => {
                     />
 
                 </div>
+                <div>
+                    <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First name</label>
+
+                    <input
+                        type="text"
+                        id="first_name"
+                        class="border border-gray-300 text-gray-900 rounded-sm w-full p-3 focus:outline-none focus:border-primary focus:ring-1 focus:ring-primary "
+                        required
+                    />
+
+                </div>
             </form>
         </div>
     );

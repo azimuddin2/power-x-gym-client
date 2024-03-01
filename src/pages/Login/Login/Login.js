e9dea94f04962ad401a0dc1812cf1647d5977950
azimuddin2 <mohammadazimuddin274@gmail.com>
2024-03-01 15:30:30 +0600

    login and signup form

--- src/pages/Login/Login/Login.js
+++ src/pages/Login/Login/Login.js
@@ -1,57 +1,56 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import useTitle from '../../../hooks/useTitle';
+import './Login.css';
 
 const Login = () => {
+    useTitle('Login');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const possword = form.password.value;
+    };
+
     return (
         <section className='my-28'>
-
-
-            <div>
-                <Link to="/">
-                    <p className="text-secondary uppercase text-2xl font-medium text-center font-family">
-                        Power <span className='text-primary ml-1'>x-gym</span>
-                    </p>
-                </Link>
-
-
-
-                <form className=' w-2/5 mx-auto'>
-
+            <Link to="/">
+                <p className="mb-6 text-secondary uppercase text-2xl font-medium text-center font-family">
+                    Power <span className='text-primary ml-1'>x-gym</span>
+                </p>
+            </Link>
+            <div className='form'>
+                {/* <h1 className='text-center text-3xl font-medium mb-6'>Login</h1> */}
+                <form onSubmit={handleSubmit}>
                     <div>
-                        <label htmlFor="first_name" className="block ml-1 mb-2 text-sm font-medium ">First Name</label>
-                        <input
-                            type="text"
-                            name='firstName'
-                            className="border border-gray-300 text-gray-900 rounded-sm w-full p-2 focus:outline-none focus:border-primary focus:ring-1 focus:ring-primary "
-                            required
-                        />
-                    </div>
-
-                    <div>
-                        <label htmlFor="email" className="block ml-1 mb-2 text-sm font-medium ">Email</label>
+                        <label htmlFor="email" className="block ml-1 mb-2 text-sm font-medium">Email</label>
                         <input
                             type="email"
-                            name='email'
+                            name="email"
                             className="border border-gray-300 text-gray-900 rounded-sm w-full p-2 focus:outline-none focus:border-primary focus:ring-1 focus:ring-primary "
                             required
                         />
                     </div>
-
-                    <div className=' flex justify-end mt-10'>
+                    <div className='mt-3'>
+                        <label htmlFor="password" className="block ml-1 mb-2 text-sm font-medium ">Password</label>
                         <input
-                            className=" w-full font-semibold uppercase py-2 text-base text-center rounded-sm bg-secondary text-neutral cursor-pointer"
-                            type="submit"
-                            value="Submit"
+                            type="password"
+                            name='password'
+                            className="border border-gray-300 text-gray-900 rounded-sm w-full p-2 focus:outline-none focus:border-primary focus:ring-1 focus:ring-primary "
+                            required
                         />
                     </div>
-
-                </form >
-
-
+                    <div className='mt-6'>
+                        <input
+                            className="w-full font-semibold uppercase py-2 text-base text-center rounded-sm bg-secondary text-neutral cursor-pointer"
+                            type="submit"
+                            value="Login"
+                        />
+                    </div>
+                </form>
+                <p className='text-center mt-5 text-secondary'>Don’t have an account? <Link to='/signup' className='text-primary'>Create an account</Link> </p>
             </div>
-
-
-
         </section>
     );
 };
--- src/pages/Login/SignUp/SignUp.js
+++ src/pages/Login/SignUp/SignUp.js
@@ -1,10 +1,65 @@
 import React from 'react';
+import useTitle from '../../../hooks/useTitle';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
+    useTitle('SignUp');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const possword = form.password.value;
+    };
+
     return (
-        <div>
-            <h1>SignUp</h1>
-        </div>
+        <section className='my-28'>
+            <Link to="/">
+                <p className="mb-6 text-secondary uppercase text-2xl font-medium text-center font-family">
+                    Power <span className='text-primary ml-1'>x-gym</span>
+                </p>
+            </Link>
+            <div className='form'>
+                <form onSubmit={handleSubmit}>
+                    <div className='mb-3'>
+                        <label htmlFor="name" className="block ml-1 mb-2 text-sm font-medium">Name</label>
+                        <input
+                            type="text"
+                            name="name"
+                            className="border border-gray-300 text-gray-900 rounded-sm w-full p-2 focus:outline-none focus:border-primary focus:ring-1 focus:ring-primary "
+                            required
+                        />
+                    </div>
+                    <div className='mb-3'>
+                        <label htmlFor="email" className="block ml-1 mb-2 text-sm font-medium">Email</label>
+                        <input
+                            type="email"
+                            name="email"
+                            className="border border-gray-300 text-gray-900 rounded-sm w-full p-2 focus:outline-none focus:border-primary focus:ring-1 focus:ring-primary "
+                            required
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="password" className="block ml-1 mb-2 text-sm font-medium ">Password</label>
+                        <input
+                            type="password"
+                            name='password'
+                            className="border border-gray-300 text-gray-900 rounded-sm w-full p-2 focus:outline-none focus:border-primary focus:ring-1 focus:ring-primary "
+                            required
+                        />
+                    </div>
+                    <div className='mt-6'>
+                        <input
+                            className="w-full font-semibold uppercase py-2 text-base text-center rounded-sm bg-secondary text-neutral cursor-pointer"
+                            type="submit"
+                            value="SignUp"
+                        />
+                    </div>
+                </form>
+                <p className='text-center mt-5 text-secondary'>Already have an account? <Link to='/login' className='text-primary'>Login</Link> </p>
+            </div>
+        </section>
     );
 };
 
